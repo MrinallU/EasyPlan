4fef8d2b76f3dbd124f44628a184eba32c842bd4
MrinallU <uma@VAL-NB27.Valensa.local>
2021-02-11 10:21:06 -0500

    Remove followers

--- src/features/user/PeopleDashboard/PeopleDashboard.jsx
+++ src/features/user/PeopleDashboard/PeopleDashboard.jsx
@@ -32,13 +32,13 @@ const PeopleDashboard = ({ followings, followers }) => {
     return (
         <Grid>
             <Grid.Column width={16}>
-                <Segment>
-                    <Header dividing content="People following me" />
-                    <Card.Group itemsPerRow={8} stackable>
-                        {followers &&
-                        followers.map(follower => <PersonCard key={follower.id} user={follower} />)}
-                    </Card.Group>
-                </Segment>
+                {/*<Segment>*/}
+                {/*    <Header dividing content="People following me" />*/}
+                {/*    <Card.Group itemsPerRow={8} stackable>*/}
+                {/*        {followers &&*/}
+                {/*        followers.map(follower => <PersonCard key={follower.id} user={follower} />)}*/}
+                {/*    </Card.Group>*/}
+                {/*</Segment>*/}
                 <Segment>
                     <Header dividing content="People I'm following" />
                     <Card.Group itemsPerRow={8} stackable>
